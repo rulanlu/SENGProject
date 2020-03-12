c87438aad95c8a382471384fffa4624afa0210fc
Rulan <rulan.lu@hotmail.com>
2020-03-12 14:11:00 -0600

    added to student menu

--- src/StudentMenu.java
+++ src/StudentMenu.java
@@ -5,10 +5,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
 
 public class StudentMenu extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField searchBar;
 
 	/**
 	 * Launch the application.
@@ -34,11 +38,24 @@ public class StudentMenu extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Student");
-		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		JLabel description = new JLabel("University of Saskatchewan scholarship system.");
+		description.setBounds(6, 6, 438, 23);
+		contentPane.add(description);
+		
+		JLabel lblNewLabel = new JLabel("Search for scholarships below.");
+		lblNewLabel.setBounds(6, 28, 298, 16);
+		contentPane.add(lblNewLabel);
+		
+		searchBar = new JTextField();
+		searchBar.setBounds(6, 70, 246, 26);
+		contentPane.add(searchBar);
+		searchBar.setColumns(10);
+		
+		JButton searchButton = new JButton("Search");
+		searchButton.setBounds(258, 70, 85, 29);
+		contentPane.add(searchButton);
 	}
-
 }
