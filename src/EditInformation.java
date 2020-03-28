0d6b398cc4d3391335f164662cbbe2d3501d921b
Rulan <rulan.lu@hotmail.com>
2020-03-27 21:46:54 -0600

    students can now edit their information

---
+++ src/EditInformation.java
@@ -0,0 +1,191 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ * Allows student to edit their own information
+ * This information is updated in the database as well
+ * @author Rulan Lu
+ *
+ */
+public class EditInformation extends JFrame {
+
+	private JPanel contentPane;
+	static String username;
+	static String password;
+	static String GPA;
+	static String faculty;
+	private JTextField nameField;
+	private JTextField gpaField;
+	private JTextField facField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EditInformation frame = new EditInformation(username, password, GPA, faculty);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * Sets up labels, buttons, etc.
+	 */
+	public EditInformation(String suser, String spass, String sgpa, String sfac) {
+		username = suser;
+		password = spass;
+		GPA = sgpa;
+		faculty = sfac;
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(200, 200, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		setLocationRelativeTo(null);
+		setTitle("University of Saskatchewan");
+		contentPane.setLayout(null);
+		
+		JLabel nameLabel = new JLabel("Student Name: ");
+		nameLabel.setBounds(9, 17, 101, 16);
+		contentPane.add(nameLabel);
+		
+		JLabel gpaLabel = new JLabel("GPA: ");
+		gpaLabel.setBounds(72, 59, 38, 16);
+		contentPane.add(gpaLabel);
+		
+		JLabel facLabel = new JLabel("Faculty:");
+		facLabel.setBounds(52, 103, 61, 16);
+		contentPane.add(facLabel);
+		
+		//shows text fields with current student information
+		nameField = new JTextField(username);
+		nameField.setBounds(109, 12, 211, 26);
+		contentPane.add(nameField);
+		nameField.setColumns(10);
+		
+		gpaField = new JTextField(GPA);
+		gpaField.setBounds(109, 54, 211, 26);
+		contentPane.add(gpaField);
+		gpaField.setColumns(10);
+		
+		facField = new JTextField(faculty);
+		facField.setBounds(109, 98, 211, 26);
+		contentPane.add(facField);
+		facField.setColumns(10);
+		
+		//if student chooses to change information
+		JButton change = new JButton("Change");
+		change.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//open the database and change the information for the student
+				try {
+		            FileInputStream fstream = new FileInputStream("src/student.txt");
+		            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+		            String line;
+		            StringBuilder fileContent = new StringBuilder();
+					
+					while ((line = br.readLine()) != null) {
+		                String sArray[] = line.split(", ");
+		                if (sArray.length > 0) {
+		                	//if line containing student is found, edit it
+		                    if (sArray[0].equals(username)) {
+		                        String newLine = nameField.getText() + ", " + password + ", " + gpaField.getText() + ", " + facField.getText();
+		                        fileContent.append(newLine);
+		                        fileContent.append("\n");
+		                    //otherwise keep it as it is
+		                    } else {
+		                        fileContent.append(line);
+		                        fileContent.append("\n");
+		                    }
+		                }
+		            }
+
+			        FileWriter fstreamWrite = new FileWriter("src/student.txt");
+		            BufferedWriter out = new BufferedWriter(fstreamWrite);
+		            out.write(fileContent.toString());
+		            out.close();
+		            
+		            //change student information in application database as well
+		            try {
+			            FileInputStream fstream2 = new FileInputStream("src/Applications.txt");
+			            BufferedReader br2 = new BufferedReader(new InputStreamReader(fstream2));
+			            String line2;
+			            StringBuilder fileContent2 = new StringBuilder();
+						
+						while ((line2 = br2.readLine()) != null) {
+			                String sArray2[] = line2.split(", ");
+			                if (sArray2.length > 0) {
+			                	//if line containing student is found, edit it
+			                    if (sArray2[0].equals(username)) {
+			                        String newLine2 = nameField.getText() + ", " + sArray2[1];
+			                        fileContent2.append(newLine2);
+			                        fileContent2.append("\n");
+			                    //otherwise keep it as it is
+			                    } else {
+			                        fileContent2.append(line2);
+			                        fileContent2.append("\n");
+			                    }
+			                }
+			            }
+
+				        FileWriter fstreamWrite2 = new FileWriter("src/Applications.txt");
+			            BufferedWriter out2 = new BufferedWriter(fstreamWrite2);
+			            out2.write(fileContent2.toString());
+			            out2.close();
+
+				    } catch (Exception ex) {
+				        System.out.println("Problem reading file.");
+				    }
+
+			    } catch (Exception ex) {
+			        System.out.println("Problem reading file.");
+			    }
+				//change variables and go back to student information
+                username = nameField.getText();
+                GPA = gpaField.getText();
+                faculty = facField.getText();
+				StudentInformation information = new StudentInformation(username);
+				information.setVisible(true);
+				setVisible(false);
+				JOptionPane.showMessageDialog(null, "Change successful", null, JOptionPane.PLAIN_MESSAGE);
+			}
+		});
+		change.setBounds(349, 243, 95, 29);
+		contentPane.add(change);
+		
+		JButton back = new JButton("Back");
+		back.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				StudentInformation information = new StudentInformation(username);
+				information.setVisible(true);
+				setVisible(false);
+			}
+		});
+		back.setBounds(9, 243, 69, 29);
+		contentPane.add(back);
+		
+	}
+}
--- src/StudentInformation.java
+++ src/StudentInformation.java
@@ -1,18 +1,33 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+/**
+ * Page for students to view their own information
+ * Students may see their username (name), GPA, and faculty. They may also make changes to this information.
+ * @author Rulan Lu
+ *
+ */
 public class StudentInformation extends JFrame {
 
 	private JPanel contentPane;
@@ -20,6 +35,8 @@ public class StudentInformation extends JFrame {
 	static String password;
 	static String GPA;
 	static String faculty;
+	static String oldLine;
+	static String newLine;
 
 	/**
 	 * Launch the application.
@@ -39,17 +56,19 @@ public class StudentInformation extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * Sets up labels, buttons, etc.
 	 */
 	public StudentInformation(String suser) {
 		username = suser;
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(200, 200, 750, 500);
+		setBounds(200, 200, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		//get student information from the database
 		try {
 			Scanner in = new Scanner(new File("src/student.txt"));
 			while (in.hasNextLine()) {
@@ -69,47 +88,34 @@ public class StudentInformation extends JFrame {
 			JOptionPane.showMessageDialog(null,"Student Database Not Found", "Error", JOptionPane.ERROR_MESSAGE);
 	    }
 		
+		//shows all student information
 		JLabel nameLabel = new JLabel("Student Name: " + username);
-		nameLabel.setBounds(79, 6, 216, 16);
+		nameLabel.setBounds(9, 6, 216, 16);
 		contentPane.add(nameLabel);
 		
 		JLabel gpaLabel = new JLabel("GPA: " + GPA);
-		gpaLabel.setBounds(79, 52, 146, 16);
+		gpaLabel.setBounds(9, 52, 146, 16);
 		contentPane.add(gpaLabel);
 		
 		JLabel facLabel = new JLabel("Faculty: " + faculty);
-		facLabel.setBounds(79, 98, 227, 16);
+		facLabel.setBounds(9, 98, 227, 16);
 		contentPane.add(facLabel);
 		
+		//if student wants to edit their information, go to editing page
 		JButton editName = new JButton("Edit");
-		editName.setBounds(0, 1, 67, 29);
+		editName.setBounds(9, 128, 91, 29);
 		editName.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				EditInformation edit = new EditInformation(username, password, GPA, faculty);
+				edit.setVisible(true);
+				setVisible(false);
 			}
 		});
 		contentPane.add(editName);
 		
-		JButton editGPA = new JButton("Edit");
-		editGPA.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		editGPA.setBounds(0, 47, 67, 29);
-		contentPane.add(editGPA);
-		
-		JButton editFaculty = new JButton("Edit");
-		editFaculty.setBounds(0, 93, 67, 29);
-		editFaculty.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		contentPane.add(editFaculty);
-		
+		//takes student back to the student menu
 		JButton backButton = new JButton("Back");
-		backButton.setBounds(6, 443, 75, 29);
+		backButton.setBounds(0, 247, 67, 25);
 		backButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				StudentMenu student = new StudentMenu();
@@ -121,7 +127,7 @@ public class StudentInformation extends JFrame {
 		contentPane.add(backButton);
 		
 		JButton confirmButton = new JButton("Finish");
-		confirmButton.setBounds(662, 443, 82, 29);
+		confirmButton.setBounds(362, 247, 82, 25);
 		confirmButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				StudentMenu student = new StudentMenu();
