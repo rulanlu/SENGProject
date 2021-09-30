91f56fe5186d8c428adfa97d435d5d9542043376
mtamkee <33907017+mtamkee@users.noreply.github.com>
2021-09-30 16:31:04 -0600

    created new file for fileReader helper function

--- src/changeStudentInformationTest.java
+++ src/changeStudentInformationTest.java
@@ -23,6 +23,8 @@ class changeStudentInformationTest {
 	@Test
 	void test() {
 		try {
+			changeStudentInformation student = new changeStudentInformation();
+			
 			String expectedArray[] = {"MattTest","4321","3.20","Science", "No"};
 			String resultArray[] = null;
 			String username = "MattTest";
@@ -32,11 +34,12 @@ class changeStudentInformationTest {
 			String GPA = "3.90";
 			String Faculty = "Education";
 			String Awarded = "No";
+			student.changeInformation(username, usernameField, password, GPA, Faculty, Awarded);
 			
 			// changing GPA and faculty
 			String newGPA = "3.20";
 			String newFaculty = "Science";
-			changeStudentInformation student = new changeStudentInformation();
+			
 			student.changeInformation(username, usernameField, password, newGPA, newFaculty, Awarded);
 			//read file and check for changes 
 			FileInputStream fstream = new FileInputStream("src/student.txt");
